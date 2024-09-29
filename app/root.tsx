68fa87a66d04ddc9ea80f530c9454a0e534fc5e4
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2024-09-29 19:46:22 +0200

    Added mailsubscription test

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,7 @@ import appStyles from '~/styles/app.css?url';
 import tailwindCss from './styles/tailwind.css?url';
 import {PageLayout} from '~/components/PageLayout';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
+import {Script} from '@shopify/hydrogen';
 
 export type RootLoader = typeof loader;
 
@@ -195,6 +196,37 @@ export function Layout({children}: {children?: React.ReactNode}) {
             </div>
           </div>
         </form> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          if (!window.mootrack) {
+            (function (t, n, e, o, a) {
+              function d(t) {
+                var n = ~~(Date.now() / 3e5),
+                  o = document.createElement(e);
+                o.async = true;
+                o.src = t + "?ts=" + n;
+                var a = document.getElementsByTagName(e)[0];
+                a.parentNode.insertBefore(o, a);
+              }
+              t.MooTrackerObject = a;
+              t[a] =
+                t[a] ||
+                function () {
+                  return t[a].q
+                    ? void t[a].q.push(arguments)
+                    : void (t[a].q = [arguments]);
+                };
+              window.attachEvent
+                ? window.attachEvent("onload", d.bind(this, o))
+                : window.addEventListener("load", d.bind(this, o), !1);
+            })(window, document, "script", "https://cdn.stat-track.com/statics/moosend-tracking.min.js", "mootrack");
+          }
+          mootrack('loadForm', '19fa460b5e484ed2b44b796c90601453');
+        `,
+          }}
+        />
+        {/* <Script>if(!window.mootrack){ !function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","https://cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack"); } mootrack('loadForm', '19fa460b5e484ed2b44b796c90601453');</Script> */}
         <ScrollRestoration nonce={nonce} />
         <Scripts nonce={nonce} />
         <div
