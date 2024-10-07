256cf729ddc1c5e6f5690217aa6c5073c6253c42
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2024-10-07 13:17:32 +0200

    Added status to subscribing to newsletter

--- app/components/NewsletterForm.tsx
+++ app/components/NewsletterForm.tsx
@@ -1,65 +1,12 @@
-// 'use client';
-
-// import {useState} from 'react';
-// import {signupToNewsletter} from '../lib/actions';
-
-// export default function NewsletterForm() {
-//   const handleSubmit = (e: any) => {
-//     e.preventDefault();
-//     const formData = new FormData(e.target);
-//     const email = formData.get('email') as string;
-//     if (!email) {
-//       return;
-//     }
-//     signup(email);
-//   };
-
-//   const signup = async (email: string) => {
-//     try {
-//       await signupToNewsletter(email);
-//       console.log('Successfully signed up!');
-//     } catch (error) {
-//       console.error('Failed to sign up:', error);
-//     }
-//   };
-
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <>
-//       <div className="m-20 p-20">
-//         <button onClick={() => setOpen(!open)}>skhjdbkjas</button>
-//         {open && (
-//           <>
-//             <h3 className="mb-4">Newsletter signup</h3>
-//             <form onSubmit={handleSubmit}>
-//               <label htmlFor="email">Email</label>
-//               <input
-//                 //   type="email"
-//                 name="email"
-//                 id="email"
-//               />
-//               <button
-//                 type="submit"
-//                 className="cursor-pointer newsletter-form__button field__button"
-//                 name="commit"
-//                 id="Subscribe"
-//                 aria-label="Subscribe"
-//               >
-//                 Submit
-//               </button>
-//             </form>
-//           </>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
+'use client';
 
 import {useModal} from './NewsletterModalContext';
 import {signupToNewsletter} from '../lib/actions';
+import {useState} from 'react';
 
 const Modal = () => {
+  const [status, setStatus] = useState<string>();
+
   const {isModalOpen, toggleModal} = useModal();
 
   const handleSubmit = (e: any) => {
@@ -73,13 +20,17 @@ const Modal = () => {
   };
 
   const signup = async (email: string) => {
+    setStatus('loading');
     try {
       await signupToNewsletter(email);
+      setStatus('success');
       console.log('Successfully signed up!');
     } catch (error) {
       console.error('Failed to sign up:', error);
+      setStatus('error');
     }
   };
+
   return (
     <>
       {isModalOpen && (
@@ -90,21 +41,12 @@ const Modal = () => {
           ></button>
           <div className="fixed pt-20 pb-10 p-4 z-40 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[80%] sm:max-w-96 rounded-lg h-auto bg-white flex-col flex items-center justify-center">
             <div className="flex flex-col items-center justify-between w-full">
-              {/* <div className="w-20 h-20 mb-3 text-primary-green">
-                <svg
-                  color="#e88a60"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                > */}
-              {/* <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
-              {/* <path
-                    color="currentColor"
-                    fill="currentColor"
-                    d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"
-                  />
-                </svg>
-              </div> */}
-              <h2 className="mb-4 text-center">Subscribe to our Newsletter</h2>
+              <h2 className="mb-3 text-center">Subscribe to our Newsletter</h2>
+              <h3 className="mb-3">
+                {status == 'loading' && 'Loading...'}
+                {status == 'success' && 'Thank you for subscribing!'}
+                {status == 'error' && 'Failed to subscribe. Please try again.'}
+              </h3>
               <button className="cursor-pointer" onClick={toggleModal}>
                 <span className="absolute right-4 top-4 w-10 h-10 flex items-center justify-center">
                   <svg
@@ -125,7 +67,7 @@ const Modal = () => {
                 Email
               </label>
               <input
-                //   type="email"
+                type="email"
                 name="email"
                 id="email"
                 placeholder="Enter your email"
